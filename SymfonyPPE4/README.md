c8534d2af019e89f5cd6f42e9c5df7524533ba95
folpentin <folpentin@gmail.com>
2017-03-16 10:34:20 +0100

    Florentin Merlet
    supp ancien symfony

--- SymfonyPPE4/README.md
+++
@@ -1,4 +0,0 @@
-SymfonyPPE4
-===========
-
-A Symfony project created on March 16, 2017, 8:23 am.
--- SymfonyPPE4/src/PPE4/AdminBundle/PPE4AdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace PPE4\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class PPE4AdminBundle extends Bundle
-{
-}
