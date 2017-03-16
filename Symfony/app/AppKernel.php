2cfaa544597573fd4c7638325f22f7294c121c32
Tomoutch <sinant@saintemarie-cholet.eu>
2017-03-16 10:58:50 +0100

    Thomas - Installation EasyAdmin

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new AppBundle\AppBundle(),
+            new PPE4\Bundle\PPE4Bundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
